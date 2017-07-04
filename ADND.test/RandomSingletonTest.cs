1bc343792433ff243a52ba9d0db527e6a32f6b80
tombombadil2323 <tombombadil23@gmail.com>
2017-07-05 01:20:49 +0200

    Add files via upload

---
+++ ADND.test/RandomSingletonTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+namespace ADND.test
+{
+    [TestFixture()]
+    public class RandomSingletonTest
+    {
+        [Test()]
+        public void ShouldNotBeInstantiatedMoreThanOnce()
+        {
+            RandomSingleton randomSingletonA = RandomSingleton.Instance();
+            RandomSingleton randomSingletonB = RandomSingleton.Instance();
+
+            Assert.AreEqual(randomSingletonA,randomSingletonB);
+
+        }
+
+		[Test()]
+		public void ShouldBeBetween1And5()
+		{
+			RandomSingleton randomSingletonA = RandomSingleton.Instance();
+			int firstRandom = randomSingletonA.Next(1, 5);
+
+			Assert.GreaterOrEqual(firstRandom, 1);
+			Assert.LessOrEqual(firstRandom, 5);
+		}
+    }
+}
